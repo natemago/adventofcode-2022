5f4d5100474143baa49d9ad6c8c1cdcf75db94e2
Pavle Jonoski <pavle.jonoski@gmail.com>
2022-12-08 07:04:41 +0100

    :bacon: :burrito: :fire: Day 1, Calorie Counting

--- day1-calorie-counting/solution.py
+++ day1-calorie-counting/solution.py
@@ -1,16 +1,6 @@
 def read_input(inpf):
-    result = []
-    elf_entries = []
     with open(inpf) as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                if elf_entries:
-                    result.append(elf_entries)
-                    elf_entries = []
-            else:
-                elf_entries.append(int(line))
-    return result
+        return [[int(line) for line in group.split('\n') if line.strip()] for group in f.read().split('\n\n')]
 
 
 def part1(elves):
@@ -18,8 +8,7 @@ def part1(elves):
 
 
 def part2(elves):
-    elves = list(sorted([sum(elf) for elf in elves], reverse=True))
-    return sum(elves[0:3])
+    return sum((sorted([sum(elf) for elf in elves], reverse=True))[0:3])
 
 
 print('Part 1:', part1(read_input('input')))
